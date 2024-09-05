a88805a0d37dc2e30f9615a7eaae8e86c095077f
unknown <diapoletroshpetjay082@gmail.com>
2024-09-05 00:19:23 -0700

    initial upload for Activity 3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/students/create', function (){
+    $student = new Student();
+    $student->first_name = 'John';
+    $student->last_name = 'Doe';
+    $student->email = 'johndoe@example.com';
+    $student->save();
+    return 'Student Created!';
+});
+
+Route::get('/students', function (){
+    $students = Student::all();
+    return $students;
+
+});
+
+Route::get('/students/update', function (){
+    $student = Student::where('email', 'johndoe@example.com')->first();
+    $student->email = 'john.doe@newmail.com';
+    $student->save();
+    return 'Student Updated!';
+});
+
+Route::get('/students/delete', function (){
+    $student = Student::where('email', 'john.doe@newmail.com')->first();
+    $student->delete();
+    return 'Student Deleted!';
+});
 
 Route::get('/', function () {
     return view('welcome');
